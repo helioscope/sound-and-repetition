1f544035025e0d28d0340149004b0753549e2000
s <11468529+helioscope@users.noreply.github.com>
2021-04-15 01:20:30 -0400

    Lumpy clay: fooling around with Material-UI & TypeScript in the familiar realm of React. Not much to see here yet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import {Button, Container} from '@material-ui/core';
+import { ThemeProvider, createMuiTheme, Theme } from '@material-ui/core/styles';
+import { cyan, teal } from '@material-ui/core/colors';
+import { ToggleButton } from '@material-ui/lab';
+
+const theme : Theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: cyan,
+    secondary: teal,
+    background: {
+      default: '#202032'
+    }
+  },
+});
+
+type AppState = {
+  selected: boolean
+};
+
+class App extends React.Component {
+  state : AppState = {
+    selected : false
+  }
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <Container>
+            <div>Container</div>
+            <div>
+              <Button variant="contained">Default contained</Button>
+              <Button variant="outlined">Default outlined</Button>
+              <Button>Default default</Button>
+            </div>
+            <div>
+              <Button color="primary" variant="contained">Primary contained</Button>
+              <Button color="primary" variant="outlined">Primary outlined</Button>
+              <Button color="primary">Primary default</Button>
+            </div>
+            <div>
+              <Button color="secondary" variant="contained">Secondary contained</Button>
+              <Button color="secondary" variant="outlined">Secondary outlined</Button>
+              <Button color="secondary">Secondary default</Button>
+            </div>
+            <div>
+              {/* Note: ToggleButton doesn't support a "color" prop. */}
+              <ToggleButton value="check"
+                  selected={this.state.selected}
+                  onChange={() => {
+                    this.setState({selected : !this.state.selected});
+                  }}>
+                Toggle
+              </ToggleButton>
+            </div>
+          </Container>
+        </div>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
